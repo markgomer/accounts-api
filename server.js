862f2aff56553f9bd563afb278ed6f6604cf42ac
Marco A S <aureliojuniorcmrj@hotmail.com>
2024-05-15 11:01:21 -0300

    Implement RESET endpoint
    * modify main file on package.json;
    * import fs and path libraries;
    * add GET route on the root;
    * add testscript;
    * fixed db.json location.

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
-const app = require('express')(); // importing express framework
+const express = require('express'); // importing express framework
+const fs = require('fs');
+const path = require('path');
 const PORT = 8080;
+
+const app = express();
+
+app.use(express.json()); // JSON parser
+
+
+// define reset endpoint
+app.post('/reset', (req, res) => {
+    const initialData = {
+        accounts: []
+    };
+
+    fs.writeFileSync(path.join(__dirname, 'data', 'db.json'), JSON.stringify(initialData, null, 2));
+    res.status(200).send('200 OK');
+});
+
+
+// Define a route for HTTP GET requests to the root URL '/'
+app.get('/', (req, res) => {
+    res.send('GET WORKED!');
+});
+
 
 // fire up API on the server
 app.listen(
