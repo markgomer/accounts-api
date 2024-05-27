a2e30e4a4f2f1526eda12606a683b80d6c72ed99
Marco A S <aureliojuniorcmrj@hotmail.com>
2024-05-27 10:59:43 -0300

    Use httpHelper on reset message

--- controllers/resetController.js
+++ controllers/resetController.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-
+const httpHelper = require('../helpers/httpHelper');
 
 function reset(req, res) {
     const initialData = { accounts: [] };
     fs.writeFileSync(path.join('data', 'db.json'), JSON.stringify(initialData, null, 2));
-    res.status(200).send('200 OK');
+    httpHelper.sendResponse(res, 200, `200 OK`);
 }
 
 module.exports = { reset };
